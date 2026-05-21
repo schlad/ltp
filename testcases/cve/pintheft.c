9b1fa0f04f679dd48d4b88765b3be9644c20b346
Sebastian Chlad <sebastian.chlad@suse.com>
2026-05-21 16:30:23 +0200

    tcp rds

--- testcases/cve/pintheft.c
+++ testcases/cve/pintheft.c
@@ -13,6 +13,7 @@
 static void setup(void)
 {
 	int fd;
+	int val;
 
 	fd = socket(AF_RDS, SOCK_SEQPACKET | SOCK_CLOEXEC, 0);
 	if (fd < 0) {
@@ -23,12 +24,20 @@ static void setup(void)
 		tst_brk(TBROK | TERRNO, "socket(AF_RDS) failed");
 	}
 
+	val = RDS_TRANS_TCP;
+	if (setsockopt(fd, SOL_RDS, SO_RDS_TRANSPORT, &val, sizeof(val))) {
+		if (errno == ENOPROTOOPT || errno == EINVAL)
+			tst_brk(TCONF | TERRNO, "RDS TCP transport is not available");
+
+		tst_brk(TBROK | TERRNO, "setsockopt(SO_RDS_TRANSPORT) failed");
+	}
+
 	SAFE_CLOSE(fd);
 }
 
 static void run(void)
 {
-	tst_res(TPASS, "RDS is available");
+	tst_res(TPASS, "RDS TCP transport is available");
 }
 
 static struct tst_test test = {
