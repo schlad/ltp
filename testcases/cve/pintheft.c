5451ec0926a1af15d28ee5e2962ccdc7b05e0aa3
Sebastian Chlad <sebastian.chlad@suse.com>
2026-05-21 16:18:34 +0200

    rds check

--- testcases/cve/pintheft.c
+++ testcases/cve/pintheft.c
@@ -3,13 +3,35 @@
  * Copyright (c) 2026 SUSE LLC
  */
 
+#include <errno.h>
+#include <sys/socket.h>
+
+#include <linux/rds.h>
+
 #include "tst_test.h"
+
+static void setup(void)
+{
+	int fd;
+
+	fd = socket(AF_RDS, SOCK_SEQPACKET | SOCK_CLOEXEC, 0);
+	if (fd < 0) {
+		if (errno == EAFNOSUPPORT || errno == ESOCKTNOSUPPORT ||
+		    errno == EPROTONOSUPPORT || errno == ENOPROTOOPT)
+			tst_brk(TCONF | TERRNO, "RDS is not available");
+
+		tst_brk(TBROK | TERRNO, "socket(AF_RDS) failed");
+	}
+
+	SAFE_CLOSE(fd);
+}
 
 static void run(void)
 {
-	tst_res(TPASS, "hello world");
+	tst_res(TPASS, "RDS is available");
 }
 
 static struct tst_test test = {
+	.setup = setup,
 	.test_all = run,
 };
